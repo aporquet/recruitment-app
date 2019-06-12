2291960d3b5b56ee35d867e2fa4eda4b4ad4cc51
aporquet <antoine.porquet34070@gmail.com>
2019-06-12 11:53:35 +0200

    add interview component

--- recruitment-app/src/app/app-routing.module.ts
+++ recruitment-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RecruitersComponent} from './components/recruiters/recruiters.component';
+import {InterviewsComponent} from './components/interviews/interviews.component';
 
 const routes: Routes = [
-    {path: 'recruiters', component: RecruitersComponent}
+    {path: 'recruiters', component: RecruitersComponent},
+    {path: 'interviews', component: InterviewsComponent}
 ];
 
 @NgModule({
---
+++ recruitment-app/src/app/components/interviews/interviews.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from '../../api.service';
+
+@Component({
+    selector: 'app-interviews',
+    templateUrl: './interviews.component.html',
+})
+export class InterviewsComponent implements OnInit {
+
+    interviews: {}[] = [];
+
+    constructor(private apiService: ApiService) {
+    }
+
+    ngOnInit(): void {
+        this.apiService.getInterviews().subscribe((response: {}[]) => {
+            for (const interview of response) {
+                this.interviews.push(interview);
+            }
+        });
+    }
+
+}
